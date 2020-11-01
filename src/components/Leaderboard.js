fbc82acbfce49d7fd8bd7012725edb3e64e1a622
guillenjs <jorgeguilln@gmail.com>
2020-11-01 16:54:07 -0500

    leader board sort in descending order

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,23 +2,40 @@ import React from 'react'
 
 export default function Leaderboard(props) {
 
+    // let randomEmoji = () => {
+    //      const emojiArr = ["🤡",
+    //      "🥴","😄","😎","👽","😈","🤖",
+    //      "😇","😜","😒","😏","🙃","🙂","🧐",
+    //      "🤯","🤓","🎃","👻","😸","😺","👩🏽‍🎤"]
+
+    //      const random = Math.floor(Math.random() * emojiArr.length);
+
+    //      return random
+    // }
+
     let renderTable = () => {
-     return   props.leaderboard.map(s=> 
-            <tr key={s.id}>
-                    <td>
-                        {s.name}
-                    </td>
-                    <td>
-                        {s.score}
-                    </td>
-                  </tr>
-        )
+        let sortedLeaderBoard = props.leaderboard.sort(function(a, b){return b.score-a.score})
+
+        
+
+        return   sortedLeaderBoard.map(s=> 
+                <tr key={s.id}>
+                        <td>
+                            {s.name}
+                        </td>
+                        <td>
+                            {s.score}
+                        </td>
+                    </tr>
+            )
     }
+
+
     return (
         <div className = "leader-board">
             <table>
                 <tr>
-                    <th>Name</th>
+                    <th className="th-name">Name</th>
                     <th>Score</th>
                 </tr>
                     {renderTable()}
